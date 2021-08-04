1c98e124581ed00a60c797384ff6651f29e080d2
lukagasic11 <lukagasic1117@gmail.com>
2021-08-04 15:58:00 -0400

    working on create function

--- contracts/SolutionsManufacturing.sol
+++ contracts/SolutionsManufacturing.sol
@@ -6,9 +6,8 @@ contract SolutionManufacturing {
     uint public ElevatorCab = 0;
     uint public Doors = 0;
     uint public Controller = 0;
-    uint public Panel = 0;
+    uint public ControlPanel = 0;
     uint public Display = 0;
-    uint public MusicSystem = 0;
     
     struct Products{
         
@@ -17,13 +16,63 @@ contract SolutionManufacturing {
         uint64 elevatorCab;
         uint64 doors;
         uint64 controller;
-        uint64 panel;
+        uint64 controlPanel;
         uint64 display;
-        uint64 musicSystem;
     }
     uint64 public productListCount = 0;
     
+    mapping(uint64 => Products) public products;
+    
+    address public myAddress = address(this);
+    
     Products[] productList;
     
+    
+    function createElevator(uint64 aluminiumBars, uint64 stainlessSteelSheets, uint64 hardware, uint64 rubberBands, uint64 lightBulbs, uint64 displayLEDs) public returns (uint64, uint64, uint64, uint64, uint64){
+        
+        //Creating Elevator Cab
+        uint64 nbElevator;
+        if (stainlessSteelSheets >= 6 && aluminiumBars >= 12 && hardware >= 4)
+        {
+            nbElevator = stainlessSteelSheets / 6;
+        }
 
+        //Creating Elevator Doors
+        uint64 nbDoors;
+        if (rubberBands >= nbElevator * 2)
+        {
+            nbDoors = nbElevator * 2 ;
+        }
+        
+        //Creating Controller
+        uint64 nbController;
+        if (lightBulbs >= nbElevator){
+            nbController = nbElevator ;
+        }
+        
+        //Creating Control Panel
+        uint64 nbControlPanel;
+        if (displayLEDs >= nbElevator){
+            nbControlPanel = nbElevator;
+        }
+        
+        //Creating Display
+        uint64 nbDisplay = nbElevator * 2 ;
+    
+        
+        //
+        Products memory new_product;
+        new_product.productId = msg.sender;
+        new_product.elevatorCab = nbElevator;
+        new_product.doors = nbDoors;
+        new_product.controller = nbController;
+        new_product.controlPanel = nbControlPanel;
+        new_product.display = nbDisplay;
+
+        //Pushing new Elevator to list
+        productListCount++;
+        products[productListCount] = new_product;
+        productList.push(new_product);
+        return (new_product.elevatorCab, new_product.doors, new_product.controller, new_product.controlPanel, new_product.display);
+    }
 }
