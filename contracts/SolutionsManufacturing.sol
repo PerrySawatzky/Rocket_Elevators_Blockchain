46157412a196547da1d1d768a0f32b23b4e3f1dc
lukagasic11 <lukagasic1117@gmail.com>
2021-08-05 15:34:29 -0400

    fixed typo

--- contracts/SolutionsManufacturing.sol
+++ contracts/SolutionsManufacturing.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.5.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SolutionManufacturing is ERC721 {
+contract SolutionsManufacturing is ERC721 {
     
     
     
