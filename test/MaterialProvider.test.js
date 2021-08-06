a4861234bd7b831f9bbd9b02d4749b8caed5ac41
emmanuelsarpong <emmanuel.sarpong12@gmail.com>
2021-08-06 02:17:11 -0400

    final changes

--- test/MaterialProvider.test.js
+++ test/MaterialProvider.test.js
@@ -2,27 +2,15 @@ const MaterialProvider = artifacts.require("MaterialProvider");
 
 contract("MaterialProvider", (accounts) => {
 
-    it("should be a quantity of 5", function(){
-        return MaterialProvider.deployed().then(function(instance){
-            return instance.getCountaluminumBars.call();
-        }).then(function(quantity){
-            assert.equal(quantity, 5, "5 was not in the contract");
+    const ProjectOffice = artifacts.require("ProjectOffice")
+
+    contract('ProjectOffice', () => {
+        
+        it ("should deploy project office properly", async() =>{
+            console.log("Address :" + ProjectOffice.address);
+            assert(ProjectOffice.address !== '');
         });
-    });
-    it("should be a quantity of 10", function(){
-        return MaterialProvider.deployed().then(function(instance){
-            return instance.getCountstainlessSteelSheets.call();
-        }).then(function(quantity){
-            assert.equal(quantity, 10, "10 was not in the contract");
-        });
-    });
-    it("should be a quantity of 9", function(){
-        return MaterialProvider.deployed().then(function(instance){
-            return instance.getCountHardware.call();
-        }).then(function(quantity){
-            assert.equal(quantity, 9, "9 was not in the contract");
-        });
-    });
+    })
     before(async () => {
         contract = await MaterialProvider.deployed();
     });
@@ -34,14 +22,4 @@ contract("MaterialProvider", (accounts) => {
         const symbol = await contract.symbol();
         assert.equal(symbol, "MATERIALPROVIDER");
     });
-    // describe ("minting", async() => {
-    
-    //     it("creates a new token", async() => {
-    //         const result = await contract.mint('aluminumBars');
-    //         const totalSupply = await contract.totalSupply();
-    //         //SUCESS
-    //         assert.equal(totalSupply, 1);
-    //         console.log(result);
-    //     });
-    // });
 });
