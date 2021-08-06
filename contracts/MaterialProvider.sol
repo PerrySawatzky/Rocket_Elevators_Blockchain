77ae31d1421cb0eb5964ece2f185774e97844727
emmanuelsarpong <emmanuel.sarpong12@gmail.com>
2021-08-06 01:49:55 -0400

    new changes to contract

--- contracts/MaterialProvider.sol
+++ contracts/MaterialProvider.sol
@@ -1,60 +1,56 @@
-// contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.8.1;
+pragma solidity >=0.4.22 <0.9.0;
 import "./ERC721.sol";
+
 
 contract MaterialProvider is ERC721 {
 
-    string[] public materials;
-    mapping(uint64 => MaterialsStruct) MaterialsStructs;
-    uint64 public materialCount;
-
-    uint64 public aluminumBars = 5;
-    uint64 public stainlessSteelSheets = 10;
-    uint64 public hardware = 9;
-    uint64 public rubberBands = 6;
-    uint64 public lightBulbs = 3;
-    uint64 public displayLEDs = 17;
-
-    struct MaterialsStruct {
-        uint64 stainlessSteelSheets;
-        uint64 hardware;
-        uint64 rubberBands;
-        uint64 lightBulbs;
-        uint64 displayLEDs;
+    constructor() ERC721("MaterialProvider", "MATERIALPROVIDER") {
     }
 
-    constructor() ERC721("MaterialProvider", "MATERIALPROVIDER") public {
+    uint public aluminiumBars = 0;
+    uint public stainlessSteelSheets = 0;
+    uint public hardware = 0;
+    uint public rubberBands = 0;
+    uint public lightBulbs = 0;
+    uint public displayLeds = 0;
+
+    struct Materials{
+        uint aluminiumBars;
+        uint stainlessSteelSheets;
+        uint hardware;
+        uint rubberBands;
+        uint lighBulbs;
+        uint displayLeds;
     }
 
-    function mint(string memory _material, uint64 aluminumBars, uint64 stainlessSteelSheets, uint64 hardware, uint64 rubberBands, uint64 lightBulbs, uint64 displayLEDs) public returns (uint64, uint64, uint64, uint64, uint64, uint64) {
-        materials.push(_material);
-        uint _id = materials.length;
-        _mint(msg.sender, _id);
-    }
+    uint public materialListCount;
 
-    function getCountaluminumBars() view public returns (uint64) {
-        return aluminumBars;
-    }
-    
-    function getCountstainlessSteelSheets() view public returns (uint64) {
-        return stainlessSteelSheets;
-    }
-    
-    function getCountHardware() view public returns (uint64) {
-        return hardware;
-    }
-    
-    function getCountRubberBands() view public returns (uint64) {
-        return rubberBands;
-    }
-    
-    function getCountlightBulbs() view public returns (uint64) {
-        return lightBulbs;
-    }
-    
-    function getCountDisplayLEDs() view public returns (uint64) {
-        return displayLEDs;
+    mapping(uint => Materials) public materials;
+    Materials[] materialList;
+
+    function createMaterials(uint amountOfShafts, uint amountOfControllers, uint amountOfDoors, uint amountOfButtons, uint amountOfDisplays) public returns (uint,uint,uint,uint,uint,uint){
+        Materials memory newMaterial;
+
+        newMaterial.aluminiumBars = amountOfShafts*16;
+        newMaterial.stainlessSteelSheets = amountOfShafts*4 + amountOfDoors;
+        newMaterial.hardware = amountOfDoors*2;
+        newMaterial.rubberBands = amountOfDoors*2;
+        newMaterial.lighBulbs = amountOfShafts*8;
+        newMaterial.displayLeds = amountOfButtons + amountOfDisplays + amountOfControllers;
+
+        aluminiumBars = newMaterial.aluminiumBars;
+        stainlessSteelSheets = newMaterial.stainlessSteelSheets;
+        hardware = newMaterial.hardware;
+        rubberBands = newMaterial.rubberBands;
+        lightBulbs = newMaterial.lighBulbs;
+        displayLeds = newMaterial.displayLeds;
+
+        materialListCount++;
+        materials[materialListCount] = newMaterial;
+        materialList.push(newMaterial);
+
+        return (newMaterial.aluminiumBars, newMaterial.stainlessSteelSheets, newMaterial.hardware, newMaterial.rubberBands, newMaterial.lighBulbs, newMaterial.displayLeds);
     }
 
     function getContractAddress()public view returns(address){
--- test/testMaterialProvider.test.js
+++ test/testMaterialProvider.test.js
@@ -1,27 +1,13 @@
-const MaterialProvider = artifacts.require("MaterialProvider");
+const MaterialProvider = artifacts.require("MaterialProvider")
 
-contract("MaterialProvider", (accounts) => {
-
-    it("should be a quantity of 5", function(){
-        return MaterialProvider.deployed().then(function(instance){
-            return instance.getCountaluminumBars.call();
-        }).then(function(quantity){
-            assert.equal(quantity, 5, "5 was not in the contract");
-        });
+contract('MaterialProvider', () => {
+    
+    it ("should deploy project office properly", async() =>{
+        console.log("Address :" + MaterialProvider.address);
+        assert(MaterialProvider.address !== '');
     });
-    it("should be a quantity of 10", function(){
-        return MaterialProvider.deployed().then(function(instance){
-            return instance.getCountstainlessSteelSheets.call();
-        }).then(function(quantity){
-            assert.equal(quantity, 10, "10 was not in the contract");
-        });
-    });
-    it("should be a quantity of 9", function(){
-        return MaterialProvider.deployed().then(function(instance){
-            return instance.getCountHardware.call();
-        }).then(function(quantity){
-            assert.equal(quantity, 9, "9 was not in the contract");
-        });
+    before(async () => {
+        contract = await MaterialProvider.deployed();
     });
     before(async () => {
         contract = await MaterialProvider.deployed();
@@ -34,14 +20,4 @@ contract("MaterialProvider", (accounts) => {
         const symbol = await contract.symbol();
         assert.equal(symbol, "MATERIALPROVIDER");
     });
-    // describe ("minting", async() => {
-    
-    //     it("creates a new token", async() => {
-    //         const result = await contract.mint('aluminumBars');
-    //         const totalSupply = await contract.totalSupply();
-    //         //SUCESS
-    //         assert.equal(totalSupply, 1);
-    //         console.log(result);
-    //     });
-    // });
-});
+})
